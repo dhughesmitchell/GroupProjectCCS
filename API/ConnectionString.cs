61d9671957cb064cdcaf8ca84299ab997f3872f7
dhughesmitchell <dhmitchell@crimson.ua.edu>
2021-04-08 17:08:44 -0500

    Connection String Updated

--- API/ConnectionString.cs
+++ API/ConnectionString.cs
@@ -2,6 +2,16 @@ namespace API
 {
     public class ConnectionString
     {
-        
+        public string cs {get; set;}
+        public ConnectionString() {
+            string server = "cis9cbtgetlk68wl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+            string database = "e3f62d2nfi32fcfa";
+            string port = "3306";
+            string userName = "pcevcp7lz1zuhjj2";
+            string password = "y6wvwvl0nsbqj2kb";
+
+            cs = $@"server={server};user={userName};database={database};port={port};password={password};";
+
+        }
     }
 }
